a8ce5d5dad17476283c48b2f7a6619a76d0736e3
Halil <halil000ibrahim@gmail.com>
2022-03-07 03:03:54 +0300

    3: add to classes setter, getter, equals, toString methods

--- urban_planner/House.java
+++ urban_planner/House.java
@@ -1,6 +1,74 @@
 package urban_planner;
 
+import java.util.Objects;
+
 public class House extends Building {
-    
+    /** Number of rooms in house */
+    private int room;
+    /** Owner's name */
+    private String owner;
+    private String color;
+
+    public House() {
+        room = 0;
+        owner = "Anonymous";
+        color = "None";
+    }
+
+    public House(int position, int length, int height, int room, String owner, String color) {
+        super(position, length, height);
+        this.room = room;
+        this.owner = owner;
+        this.color = color;
+    }
+
+    public int getRoom() {
+        return this.room;
+    }
+
+    public void setRoom(int room) {
+        this.room = room;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof House)) {
+            return false;
+        }
+        House house = (House) o;
+        return super.equals(house) && (room == house.room && owner.equals(house.owner) && color.equals(house.color));
+    }
+
+    @Override
+    public int hashCode() {
+        return (31 * super.hashCode() + Objects.hash(room, owner, color));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "{" +
+                " room=" + getRoom() + ", " +
+                "owner=" + getOwner() + ", " +
+                "color=" + getColor() +
+                "}";
+    }
 
 }
--- urban_planner/Office.java
+++ urban_planner/Office.java
@@ -1,6 +1,60 @@
 package urban_planner;
 
+import java.util.Objects;
+
 public class Office extends Building {
-    
+    private String jobType;
+    private String owner;
+
+    public Office() {
+        jobType = "None";
+        owner = "Anonymous";
+    }
+
+    public Office(int position, int length, int height, String jobType, String owner) {
+        super(position, length, height);
+        this.jobType = jobType;
+        this.owner = owner;
+    }
+
+    public String getJobType() {
+        return this.jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public String getOwner() {
+        return this.owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Office)) {
+            return false;
+        }
+        Office office = (Office) o;
+        return super.equals(office) && (jobType.equals(office.jobType) && owner.equals(office.owner));
+    }
+
+    @Override
+    public int hashCode() {
+        return (31 * super.hashCode() + Objects.hash(jobType, owner));
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "{" +
+                " jobType=" + getJobType() + ", " +
+                "owner=" + getOwner() + "'" +
+                "}";
+    }
 
 }

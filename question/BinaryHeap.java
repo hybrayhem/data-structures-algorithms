e41c083a1eb836041a1be545bfe2f2540bdecca7
Halil <halil000ibrahim@gmail.com>
2022-04-13 20:49:22 +0300

    5: initial commit

---
+++ question/BinaryHeap.java
@@ -0,0 +1,9 @@
+package question;
+
+public class BinaryHeap {
+
+    public BinaryHeap() {
+        System.out.printf("%n/* ------------------------------- BinaryHeap ------------------------------- */%n%n");
+    }
+
+}
---
+++ question/BinarySearchTree.java
@@ -0,0 +1,9 @@
+package question;
+
+public class BinarySearchTree {
+
+    public BinarySearchTree() {
+        System.out.printf("%n/* ---------------------------- BinarySearchTree ---------------------------- */%n%n");
+    }
+
+}
